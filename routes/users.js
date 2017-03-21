17d3a4476e033cfc890681fa7bca90d1a8294430
carlos andres figueroa hernandez <carlosfh87@gmail.com>
2017-03-21 09:04:45 -0500

    checking length

--- routes/users.js
+++ routes/users.js
@@ -43,19 +43,19 @@ router.get('/', function(req, res, next) {
 });
 
 function apiresponse(err, data, reponse) {
-  // usersData = usersData.concat(data.users);
+  usersData = usersData.concat(data.users);
   users[users.current].cursor = data.next_cursor;
 
   console.log("apiresponse data.next_cursor:", data.next_cursor)
   console.log("apiresponse data:", data.users.length)
   console.log("cursor:", users[users.current].cursor )
 
-  // if(data.next_cursor != 0){
-  //   getFollowers(users[users.current].screen_name, data.next_cursor, apiresponse)
-  // }else{
-  //   users.res.send(usersData);
-  // }
-  users.res.send(data);
+  if(data.next_cursor != 0){
+    getFollowers(users[users.current].screen_name, data.next_cursor, apiresponse)
+  }else{
+    users.res.send(usersData);
+  }
+  // users.res.send(data);
 }
 
 function getFollowers (screen_name, cursor, callback) {
