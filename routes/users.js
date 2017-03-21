dfebd7ae9f8b8a942fe432a7cb6c46f73aa41231
carlos andres figueroa hernandez <carlosfh87@gmail.com>
2017-03-20 20:28:34 -0500

    test twitter api

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: 'Mfr2N91xC1JJVWvcjthsM7j1X',
+  consumer_secret: 'GMrRn4I691mVxiha6YGHp4Wn1RXfLmZIN53tRW8iqARg49uV1M',
+  access_token_key: '285409171-uXwaLMhbpNmtm57qKcpq1lAOhhb1xDavU7hjsGol',
+  access_token_secret: 'Gmeb7l8GYwbLy7xyB57hMn6cQjBLvBQLP3m6co2epRLu9'
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log("data query :",req.query);
-  var users = {
-    user1: req.query.var1,
-    user2: req.query.var2
-  }
-  res.send(users);
+  var params = {screen_name: 'nodejs'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      res.send(tweets);
+    }
+  });
+  // var users = {
+  //   user1: req.query.var1,
+  //   user2: req.query.var2
+  // }
+  // res.send(users);
 });
 
 module.exports = router;
