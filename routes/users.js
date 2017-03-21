427ebe8fa8e973f91579c2aceb9a947f25e9dffb
carlos andres figueroa hernandez <carlosfh87@gmail.com>
2017-03-20 21:35:19 -0500

    twitter lib removed

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Twitter = require('twitter');
-var Twit = require('twit')
-
-var client = new Twitter({
-  consumer_key: 'Mfr2N91xC1JJVWvcjthsM7j1X',
-  consumer_secret: 'GMrRn4I691mVxiha6YGHp4Wn1RXfLmZIN53tRW8iqARg49uV1M',
-  access_token_key: '285409171-uXwaLMhbpNmtm57qKcpq1lAOhhb1xDavU7hjsGol',
-  access_token_secret: 'Gmeb7l8GYwbLy7xyB57hMn6cQjBLvBQLP3m6co2epRLu9'
-});
+var Twit = require('twit');
 
 var client_2 = new Twit({
   consumer_key:         'Mfr2N91xC1JJVWvcjthsM7j1X',
